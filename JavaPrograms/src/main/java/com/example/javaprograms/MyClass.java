0bda89f9f0d2226a0113dcd4fc7191a3fc7dea15
sujitamin <sujit.amin@mirraw.com>
2020-04-02 12:24:50 +0530

    intial commit

---
+++ JavaPrograms/src/main/java/com/example/javaprograms/MyClass.java
@@ -0,0 +1,16 @@
+package com.example.javaprograms;
+
+import static java.lang.System.out;
+
+public class MyClass {
+    public static void main (String[] args) {
+        out.println("This is Sujit Amin");
+        for (int i = 0 ; i < 101; i++) {
+            out.println(i);
+        }
+        heroOne();
+    }
+    private static void heroOne() {
+        out.println("Hero this is Ranveer Singh");
+    }
+}
---
+++ JavaPrograms/src/main/java/com/example/javaprograms/NewTest.java
@@ -0,0 +1,8 @@
+package com.example.javaprograms;
+
+public class NewTest {
+    public static void main (String[] args) {
+        System.out.println("Balle Balle");
+    }
+}
+
